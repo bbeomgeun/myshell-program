d7f0aeacdf62a6e9212c033c65aefee8227f8b80
bbeomgeun <miki30808@gmail.com>
2021-05-02 20:59:50 +0900

    feat fork+execvp

--- myshell.c
+++ myshell.c
@@ -2,19 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define MAX 100
+
+int strParsing(char * list[], char * command, const char * delimeter){
+	int cnt = 0;
+	char * result;
+	result = strtok(command, delimeter);
+	while(result != NULL){
+		list[cnt++] = result;
+		result = strtok(NULL, delimeter);
+	}
+	list[cnt] = NULL;
+	return cnt;
+}
 
 int main() {
 	char str[MAX];
+
 	while(1){
 		printf("12161104_shell$" );
 		fgets(str, MAX-1, stdin); // fgets는 끝에 null 저장(한칸비워야함)
-		
-		// 문자열 토큰화
-		char delimeter[] = " ";
-		char parsing[3][16];
-		char *result;
-		int i = 0;
+		str[strlen(str)-1]='\0';
+
+		char * delimeter = " ";
+		char * parsing[MAX];
 
 		if(strncmp(str, "quit", 4) == 0){
 			printf("myshell developed by \"박범근(12161104)\"\n");
@@ -27,19 +41,22 @@ int main() {
 			printf("DESCRIPTION~~\n");
 		}
 		else{
-			result = strtok(str, delimeter);
+			int count = strParsing(parsing, str, " ");
+			
+			pid_t pid;
 
-			while(result != NULL){
-				strcpy(parsing[i++], result);
-				result = strtok(NULL, delimeter);
-			}		
-			for(int i = 0 ; i < 3 ; i++)
-				printf("%s", parsing[i]);
-		}
-
-		
-		
-
+			switch(pid = fork()){
+				case -1 :
+					printf("fork failed");
+					break;
+				case 0:
+					execvp (parsing[0], parsing);
+					break;
+				default:
+					wait((int *)0);
+					printf("ls ompleted\n");
+			}
+		}	
 	}
 }
 
